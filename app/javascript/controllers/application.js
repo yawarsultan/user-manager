e5f6b8f4c8d5a494d526bafa2bac2b4c39322004
yawar sultan <sultanyawar@gmail.com>
2024-02-29 17:15:23 +0500

    remove multiselect

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import { Multiselect } from '@wizardhealth/stimulus-multiselect'
 
 const application = Application.start()
-application.register('multiselect', Multiselect)
 
 // Configure Stimulus development experience
 application.debug = false
